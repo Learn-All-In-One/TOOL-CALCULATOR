5233e6859cf119f75e20caedc37efed03117c47a
mrliaoxiang <2523441592@qq.com>
2021-02-06 19:31:21 +0800

    2021.2.6-简易版

---
+++ include/calculator.h
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+#include <windows.h>
+#include "judge.h"
+
+using namespace std;
+
+class calculator;
+void choose_workmethod(int argc, char **argv);
---
+++ include/judge.h
@@ -0,0 +1,5 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+bool my_is_degital( const string str );
